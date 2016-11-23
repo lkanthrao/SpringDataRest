7324434f9446a66757d3b331200cb56bbe26e285
Lakshmikanth Rao <lkanthrao@gmail.com>
2016-11-23 06:32:21 +0530

    added few comments

--- src/main/java/com/example/controller/MyFirstController.java
+++ src/main/java/com/example/controller/MyFirstController.java
@@ -6,18 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // to expose as a rest service, advantage is no need to add
-				// request and responce body
-// but in controller we need to add both
+@RestController
+/* to expose as a rest service, advantage is no need to add request and responce body but in controller we need to add both */
 public class MyFirstController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String greet() {
+    @RequestMapping(value = "/", method = RequestMethod.GET) // actualy link http://localhost:9090/sg
+    public String greet() {
 
-		Calendar calendar = Calendar.getInstance();
-		int hour = calendar.get(Calendar.HOUR_OF_DAY);
-		return (hour < 12) ? "Good Morning" : "Good afternoon";
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        return (hour < 12) ? "Good Morning" : "Good afternoon";
 
-	}
+    }
 
 }
